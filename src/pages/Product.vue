88136c11f6ec8a762c433de68aafaf8e21433324
gor-arnaud <arnaud.michoud@gameofroom.fr>
2023-01-29 22:18:07 +0100

    Product list Cart management Routes

---
+++ src/pages/Product.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <header></header>
+  </div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "Product",
+  data() {
+    return {
+      product: {},
+    };
+  },
+  props: {
+    productId: {
+      type: Number,
+      default: 0,
+    },
+  },
+  computed: {
+    dataUrl: function () {
+      return `https://dummyjson.com/products/${this.productId}`;
+    },
+  },
+  methods: {
+    fetchProduct: function () {
+      axios
+        .get(this.dataUrl)
+        .then(({ data }) => (this.product = data))
+        .catch((error) => console.log(error));
+    },
+  },
+  mounted() {
+    this.fetchProduct();
+  },
+};
+</script>
